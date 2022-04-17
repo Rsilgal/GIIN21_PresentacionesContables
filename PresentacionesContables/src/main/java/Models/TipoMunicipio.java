b59335245cadf4a36d1b21288b91c0e98c07da3d
Rsilgal <rsilgal@gmail.com>
2022-04-17 19:47:40 +0200

    Add TipoMunicipio and TipoDocumentacion

---
+++ PresentacionesContables/src/main/java/Models/TipoMunicipio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author rsilvente
+ */
+public enum TipoMunicipio {
+    Municipio1,
+    Municipio2,
+    Municipio3,
+    Municipio4
+}
