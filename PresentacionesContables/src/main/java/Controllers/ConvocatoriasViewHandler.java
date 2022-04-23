1609135fddbdd137e87b70e19b01ab4e4a10b7a2
Rsilgal <rsilgal@gmail.com>
2022-04-23 18:50:46 +0200

    Add basic struct of actionPerformed

--- PresentacionesContables/src/main/java/Controllers/ConvocatoriasViewHandler.java
+++ PresentacionesContables/src/main/java/Controllers/ConvocatoriasViewHandler.java
@@ -5,6 +5,7 @@
 package Controllers;
 
 import Views.Convocatorias.*;
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 /**
@@ -32,7 +33,7 @@ public class ConvocatoriasViewHandler implements ActionListener {
 
     public void setDelete(Delete delete) {
         this.delete = delete;
-//        this.delete.
+        this.delete.btnBorrar.addActionListener(this);
     }
 
     public Read getRead() {
@@ -41,6 +42,7 @@ public class ConvocatoriasViewHandler implements ActionListener {
 
     public void setRead(Read read) {
         this.read = read;
+        this.read.btnVolver.addActionListener(this);
     }
 
     public Update getUpdate() {
@@ -49,10 +51,29 @@ public class ConvocatoriasViewHandler implements ActionListener {
 
     public void setUpdate(Update update) {
         this.update = update;
+        this.update.btnConfirmar.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        Component component = (Component) e.getSource();
+        if (component.getParent() == this.create) {
+            if (component == this.create.btnConfirmar) {
+                
+            }
+        } else if (component.getParent() == this.delete) {
+            if (component == this.delete.btnBorrar) {
+                
+            }
+        } else if (component.getParent() == this.read) {
+            if (component == this.read.btnVolver) {
+                
+            }
+        } else if (component.getParent() == this.update) {
+            if (component == this.update.btnConfirmar) {
+                
+            }
+        }
         
     }
     
