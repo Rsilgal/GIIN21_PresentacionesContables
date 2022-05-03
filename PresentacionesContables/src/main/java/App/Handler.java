0fc7869a66a69895c7fba4935efc0ac9b1ed573d
Rsilgal <rsilgal@gmail.com>
2022-05-03 10:46:54 +0200

    Add JavaDoc to App Package

--- PresentacionesContables/src/main/java/App/Handler.java
+++ PresentacionesContables/src/main/java/App/Handler.java
@@ -4,18 +4,9 @@
  */
 package App;
 
-import Controllers.AdministradorViewHandler;
-import Controllers.ConvocatoriasViewHandler;
-import Controllers.CuentadanteViewHandler;
-import Controllers.FiscalGeneralViewHandler;
-import Controllers.FiscalViewHandler;
-import Controllers.LoginController;
-import Controllers.MunicipiosViewHandler;
-import Controllers.PresentacionesViewHandler;
-import Controllers.UsuariosViewHandler;
-
 /**
- *
+ * Clase usada para generar y controlar la instanciación de los manejadores de
+ * cada una de las Vistas de la aplicación.
  * @author rsilvente
  */
 public class Handler {
@@ -32,6 +23,11 @@ public class Handler {
 
     private static Handler miHandler;
 
+    /**
+     * Permite obtener el Handler de las ventanas de la aplicación. En el caso 
+     * de que no exista instancia de dicho Handler, se genera una.
+     * @return El Handler de la aplicación.
+     */
     public static Handler getHandler() {
         if (miHandler == null) {
             miHandler = new Handler();
@@ -46,86 +42,186 @@ public class Handler {
         user = new Controllers.UsuariosViewHandler();
     }
 
+    /**
+     * Genera la instancia del manejador de la ventana del Administador.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createAdministradorHandler(Views.AdministradorView view) {
         admin = new Controllers.AdministradorViewHandler(view);
     }
-
+    
+    /**
+     * Genera la instancia del manejador de la ventana del Cuentadante.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createCuentadanteHandler(Views.CuentadanteView view) {
         cuent = new Controllers.CuentadanteViewHandler(view);
     }
 
+    /**
+     * * Genera la instancia del manejador de la ventana del Fiscal General.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createFiscalGeneralHandler(Views.FiscalGeneralView view) {
         fisGen = new Controllers.FiscalGeneralViewHandler(view);
     }
 
+    /**
+     * Genera la instancia del manejador de la ventana del Fiscal.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createFiscal(Views.FiscalView view) {
         fis = new Controllers.FiscalViewHandler(view);
     }
 
+    /**
+     * Genera la instancia del manejador de la ventana de Login.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createLogin(Views.LoginView view) {
         login = new Controllers.LoginController(view);
     }
 
+    /**
+     * Genera la instancia del manejador de la ventana de Creacion de 
+     * Convocatorias.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createConvocatorias_C(Views.Convocatorias.Create view) {
         conv.setCreate(view);
     }
     
+    /**
+     * Genera la instancia del manejador de la ventana para Eliminar 
+     * Convocatorias.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createConvocatorias_D(Views.Convocatorias.Delete view) {
         conv.setDelete(view);
     }
     
+    /**
+     * Genera la instancia del manejador de la ventana para Leer los datos de 
+     * una Convocatoria.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createConvocatorias_R(Views.Convocatorias.Read view) {
         conv.setRead(view);
     }
     
+    /**
+     * Genera la instancia del manejador de la ventana para Actualizar los 
+     * datos de una Convocatoria.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createConvocatorias_U(Views.Convocatorias.Update view) {
         conv.setUpdate(view);
     }
     
+    /**
+     * Genera la instancia del manejador de la ventana para Crear un nuevo 
+     * Municipio.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createMunicipios_C(Views.Municipios.Create view) {
         mun.setCreate(view);
     }
     
+    /**
+     * Genera la instancia del manejador de la ventana para Eliminar un  
+     * Municipio.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createMunicipios_D(Views.Municipios.Delete view) {
         mun.setDelete(view);
     }
     
+    /**
+     * Genera la instancia del manejador de la ventana para Leer los datos 
+     * de un Municipio.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createMunicipios_R(Views.Municipios.Read view) {
         mun.setRead(view);
     }
     
+    /**
+     * Genera la instancia del manejador de la ventana para Actualizar los 
+     * datos de un Municipio.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createMunicipios_U(Views.Municipios.Update view) {
         mun.setUpdate(view);
     }
     
+    /**
+     * Genera la instancia del manejador de la ventana para Crear una nueva 
+     * Presentación.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createPresentaciones_C(Views.Presentaciones.Create view) {
         pres.setCreate(view);
     }
     
+    /**
+     * Genera la instancia del manejador de la ventana para Eliminar una
+     * Presentacion.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createPresentaciones_D(Views.Presentaciones.Delete view) {
         pres.setDelete(view);
     }
     
+    /**
+     * Genera la instancia del manejador de la ventana para Leer una 
+     * Presentacion.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createPresentaciones_R(Views.Presentaciones.Read view) {
         pres.setRead(view);
     }
     
+    /**
+     * Genera la instancia del manejador de la ventana para Actualizar una 
+     * Presentacion.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createPresentaciones_U(Views.Presentaciones.Update view) {
         pres.setUpdate(view);
     }
     
+    /**
+     * Genera la instancia del manejador de la ventana para Crear un nuevo 
+     * Usuario.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createUsuarios_C(Views.Usuarios.Create view) {
         user.setCreate(view);
     }
     
+    /**
+     * Genera la instancia del manejador de la ventana para Eliminar un 
+     * Usuario.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createUsuarios_D(Views.Usuarios.Delete view) {
         user.setDelete(view);
     }
     
+    /**
+     * Genera la instancia del manejador de la ventana para Leer los datos  
+     * de un Usuario.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createUsuarios_R(Views.Usuarios.Read view) {
         user.setRead(view);
     }
     
+    /**
+     * Genera la instancia del manejador de la ventana para Actualizar los 
+     * datos de un Usuario.
+     * @param view Vista sobre la que actua el Handler.
+     */
     public void createUsuarios_U(Views.Usuarios.Update view) {
         user.setUpdate(view);
     }
--- PresentacionesContables/src/main/java/App/Router.java
+++ PresentacionesContables/src/main/java/App/Router.java
@@ -41,6 +41,11 @@ public class Router {
     
     private static Router miRouter;
 
+    /**
+     * Permite obtener el controlador de la Navegación de la aplicación. En 
+     * el caso de que no exista ninguna instancia de este, es generada.
+     * @return Devuelve el controlador de la navegación de la aplicación.
+     */
     public static Router getRouter() {
         if (miRouter == null) {
             miRouter = new Router();
@@ -55,6 +60,10 @@ public class Router {
         this.login.setLocationRelativeTo(null);
     }
 
+    /**
+     * Método el cual genera todas las instancias necesarias para el correcto 
+     * funcionamiento de la aplicación por parte del Administrador.
+     */
     public void generatedViewsForAdmin() {
         this.admin = new Views.AdministradorView();
         Handler.getHandler().createAdministradorHandler(admin);
@@ -100,6 +109,10 @@ public class Router {
         Handler.getHandler().createUsuarios_U(user_update);
     }
 
+    /**
+     * Método el cual genera todas las instancias necesarias para el correcto 
+     * funcionamiento de la aplicación por parte del Cuentadante.
+     */
     public void generatedViewsForCuentadante() {
         this.cuentadante = new Views.CuentadanteView();
         Handler.getHandler().createCuentadanteHandler(cuentadante);
@@ -114,6 +127,10 @@ public class Router {
         Handler.getHandler().createPresentaciones_R(pres_read);
     }
 
+    /**
+     * Método el cual genera todas las instancias necesarias para el correcto 
+     * funcionamiento de la aplicación por parte del FiscalGeneral.
+     */
     public void generatedViewsForFiscalGeneral() {
         this.fiscalGeneral = new Views.FiscalGeneralView();
         Handler.getHandler().createFiscalGeneralHandler(fiscalGeneral);
@@ -136,6 +153,10 @@ public class Router {
         Handler.getHandler().createPresentaciones_U(pres_update);
     }
 
+    /**
+     * Método el cual genera todas las instancias necesarias para el correcto 
+     * funcionamiento de la aplicación por parte del Fiscal.
+     */
     public void generatedViewsForFiscal() {
         this.fiscal = new Views.FiscalView();
         Handler.getHandler().createFiscal(fiscal);
@@ -150,96 +171,191 @@ public class Router {
         Handler.getHandler().createPresentaciones_R(pres_read);
     }
 
+    /**
+     * Método cuyo objetivo es realizar el cambio de la ventana en al que se 
+     * encuentra el usuario (Actual) a la ventana definida como Objetivo.
+     * @param actual Ventana desde la que se inicia la navegación.
+     * @param destino Ventana a la cual se quiere navegar.
+     */
     public void navigateTo(JFrame actual, JFrame destino) {
         actual.setVisible(false);
         lastFrame = actual;
         destino.setVisible(true);
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana de Administrador.
+     */
     public Views.AdministradorView getAdmin() {
         return admin;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana de Cuentadante.
+     */
     public Views.CuentadanteView getCuentadante() {
         return cuentadante;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana de Fiscal General.
+     */
     public Views.FiscalGeneralView getFiscalGeneral() {
         return fiscalGeneral;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana de Fiscal.
+     */
     public Views.FiscalView getFiscal() {
         return fiscal;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana de Login.
+     */
     public Views.LoginView getLogin() {
         return login;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana de Crear una Convocatoria.
+     */
     public Views.Convocatorias.Create getConv_create() {
         return conv_create;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana de Eliminar una Convocatoria.
+     */
     public Views.Convocatorias.Delete getConv_delete() {
         return conv_delete;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana para Leer una Convocatoria.
+     */
     public Views.Convocatorias.Read getConv_read() {
         return conv_read;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana para Actualizar una Convocatoria.
+     */
     public Views.Convocatorias.Update getConv_update() {
         return conv_update;
     }
-
+    
+    /**
+     * 
+     * @return Devuelve la ventana para Crear un Municipio.
+     */
     public Views.Municipios.Create getMun_create() {
         return mun_create;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana para Eliminar un Municipio.
+     */
     public Views.Municipios.Delete getMun_delete() {
         return mun_delete;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana para Leer un Municipio.
+     */
     public Views.Municipios.Read getMun_read() {
         return mun_read;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana para Actualizar un Municipio.
+     */
     public Views.Municipios.Update getMun_update() {
         return mun_update;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana para Crear una Presentacion.
+     */
     public Views.Presentaciones.Create getPres_create() {
         return pres_create;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana para Eliminar una Presentacion.
+     */
     public Views.Presentaciones.Delete getPres_delete() {
         return pres_delete;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana para Leer una Presentacion.
+     */
     public Views.Presentaciones.Read getPres_read() {
         return pres_read;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana para Actualizar una Presentacion.
+     */
     public Views.Presentaciones.Update getPres_update() {
         return pres_update;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana para Crear un Usuario.
+     */
     public Views.Usuarios.Create getUser_create() {
         return user_create;
     }
-
+    
+    /**
+     * 
+     * @return Devuelve la ventana para Eliminar un Usuario.
+     */
     public Views.Usuarios.Delete getUser_delete() {
         return user_delete;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana Leer un Usuario.
+     */
     public Views.Usuarios.Read getUser_read() {
         return user_read;
     }
 
+    /**
+     * 
+     * @return Devuelve la ventana para Actualizar un Usuario.
+     */
     public Views.Usuarios.Update getUser_update() {
         return user_update;
     }
-
+    
+    /**
+     * 
+     * @return Devuelve la última ventana desde la que se ha iniciado la 
+     * navegación.
+     */
     public JFrame getLastFrame() {
         return lastFrame;
     }
